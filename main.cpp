8bf490d4f4795de310e86fe529463a00c97e943d
FilipPrzygoda <96376021+FilipPrzygoda@users.noreply.github.com>
2023-03-23 19:22:11 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,14 +5,27 @@
 #include <vector>
 #include <tuple>
 #include <sstream>
+#include <cassert>
+
 
 using namespace std;
+
+// Funkcja bfs realizująca przeszukiwanie wszerz
+// Funkcja valid sprawdza czy punkt nie jest poza planszą
+// Funkcja seting_data ładuje dane poczotkowe z pliku
+// Funkcja test testuje dla przyładowych danych
+// sx, sy - współrzędne startowe
+// ex, ey - współrzędne końcowe
+// vis - tablica odwiedzin
+// dist - tablica odległości
+// n, m - wymiary planszy
 
 bool valid(int x, int y,int n, int m) {
     return (x >= 0 && x < n && y >= 0 && y < m);
 }
 
 int bfs(int sx, int sy, int ex, int ey, vector<vector<bool>>& vis, vector<vector<int>>& dist, int n, int m) {
+    // Stałe zmienne reprezentujące przesunięcie w osiach x i y dla 8 sąsiadów
     const int dx[] = {1, 1, -1, -1, 2, 2, -2, -2};
     const int dy[] = {2, -2, 2, -2, 1, -1, 1, -1};
 
@@ -43,14 +56,13 @@ int bfs(int sx, int sy, int ex, int ey, vector<vector<bool>>& vis, vector<vector
 
     return -1;
 }
-
-int main() {
+int seting_data(string path){
     int n, m, sx, sy, ex, ey;
 
-    ifstream inputFile("../dane.txt");
+    ifstream inputFile(path);
     if (!inputFile) {
         cerr << "Nie można otworzyć pliku!" << endl;
-        return 1;
+        return -1;
     }
 
     string line;
@@ -78,13 +90,38 @@ int main() {
     }
 
     inputFile.close();
+    return bfs(sx, sy, ex, ey, vis, dist, n, m);
+}
+int test() {
+    // Test dla dane1.txt
+    string path1 = "../test/dane1.txt";
+    assert(seting_data(path1) == 3);
 
-    int ans = bfs(sx, sy, ex, ey, vis, dist, n, m);
+    // Test dla dane2.txt
+    string path2 = "../test/dane2.txt";
+    assert(seting_data(path2) == -1);
+
+    // Test dla dane3.txt
+    string path3 = "../test/dane3.txt";
+    assert(seting_data(path3) == 5);
+
+    // Test dla dane4.txt
+    string path4 = "../test/dane4.txt";
+    assert(seting_data(path4) == -1);
+
+    cout << "Wszystkie testy zakończone sukcesem!" << endl;
+    return 0;
+}
+
+int main() {
+
+    int ans = seting_data("../dane.txt");
     if (ans == -1) {
         cout << "NIE" << endl;
     } else {
         cout << ans << endl;
     }
 
+    test();
     return 0;
 }
